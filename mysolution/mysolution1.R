b6b1c0c17e5acf813d37a3f9e24a327779f5f625
Michał Wąsiński <micwasi2003@gmail.com>
2025-11-23 16:52:31 +0100

    update AppURL.txt with personal details and add mysolution1.R

---
+++ mysolution/mysolution1.R
@@ -0,0 +1,64 @@
+library(igraph)
+
+set.seed(44)
+
+# 2. Wygeneruj sieć Erdős-Rényi o stu wierzchołkach i prawdopodobieństwie krawędzi = 0.05.
+n <- 100
+p <- 0.05
+g <- sample_gnp(n = n, p = p, directed = FALSE, loops = FALSE)
+
+# 3. Wydrukuj podsumowanie grafu - czy graf jest ważony?
+print(g)
+cat("Czy graf jest ważony? ->", "weight" %in% edge_attr_names(g), "\n\n")
+# Graf nie jest ważony
+
+# 4. Wylistuj wszystkie wierzchołki i krawędzie.
+nodes <- V(g)
+edges <- as_edgelist(g)
+
+cat("Wierzchołki:\n")
+print(nodes)
+
+cat("\nKrawędzie:\n")
+print(edges)
+
+# 5. Ustaw wagi wszystkich krawędzi na losowe z zakresu 0.01 do 1
+E(g)$weight <- runif(ecount(g), 0.01, 1)
+
+# 6. Wydrukuj ponownie podsumowanie grafu - czy teraz graf jest ważony?
+print(g)
+cat("Czy graf jest ważony? ->", "weight" %in% edge_attr_names(g), "\n\n")
+# Graf jest ważony
+
+cat("Krawędzie z wagami:\n")
+print(cbind(edges, weight = round(E(g)$weight, 3)))
+
+# 7. Jaki jest stopień każdego węzła? Następnie stwórz histogram stopni węzłów.
+deg <- degree(g)
+cat("\nStopnie węzłów:\n")
+print(deg)
+
+hist(deg,
+     main = "Histogram stopni węzłów",
+     xlab = "Stopień",
+     col = "lightblue",
+     border = "black")
+
+# 8. Ile jest klastrów (connected components) w grafie?
+comp <- components(g)
+cat("\nLiczba spójnych składowych:", comp$no, "\n")
+# Są 3 składowe
+
+# 9. Zwizualizuj graf w taki sposób, aby rozmiar węzłów odpowiadał mierze PageRank.
+pr <- page_rank(g, weights = E(g)$weight)$vector
+node_sizes <- 400 * pr
+
+plot(
+  g,
+  vertex.size = node_sizes,
+  vertex.label = NA,
+  vertex.color = "skyblue",
+  edge.color = "grey40",
+  main = "Wizualizacja grafu (rozmiar węzłów = PageRank)"
+)
+
